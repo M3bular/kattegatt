73a56a95dad82f30e849a48b1400bee2810afdd8
Mebular <melkerohmes@gmail.com>
2026-09-16 08:52:54 +0200

    programmering 8:10-9:20

--- programmering/Övningar - 08. while-loopar/uppgift_09.py
+++ programmering/Övningar - 08. while-loopar/uppgift_09.py
@@ -1,1 +1,24 @@
 # Uppgift 9
+
+val=0
+tal1 = int(0)
+tal2 = int(0)
+while val != 3:
+    print("\n\n1. Addera två tal")
+    print("2. Subtrahera två tal")
+    print("3. Avsluta")
+    val = int(input("Vad väljer du?"))
+    if val == 1:
+        tal1 = int(input("tal1 "))
+        tal2 = int(input("tal2 "))
+        print(tal1 + tal2)
+        val=0
+    elif val == 2:
+        tal1 = int(input("tal1 "))
+        tal2 = int(input("tal2 "))
+        print(tal1 - tal2)
+        val = 0
+    elif val == 3:
+        print("goodbaj")
+    else:
+        print("talet finns inte med yani")
--- programmering/Övningar - 08. while-loopar/uppgift_10.py
+++ programmering/Övningar - 08. while-loopar/uppgift_10.py
@@ -1,1 +1,14 @@
 # Uppgift 10
+import random
+tal = random.randint(1, 100)
+gissning = 0
+
+while gissning != tal:
+	gissning = int(input("Gissa ett tal mellan 1 och 100: "))
+
+	if gissning < tal:
+		print("Talet är för litet.")
+	elif gissning > tal:
+		print("Talet är för stort.")
+	else:
+		print("Rätt gissat!")
